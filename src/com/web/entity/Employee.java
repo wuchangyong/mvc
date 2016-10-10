0c5f21c05752d1a5607b8d359151bcebcff2ecb2
wuchangyong <9578987@qq.com>
2016-10-10 16:48:04 +0800

    oopopopopopo

--- src/com/web/entity/Employee.java
+++ src/com/web/entity/Employee.java
@@ -3,29 +3,49 @@ package com.web.entity;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @SuppressWarnings("serial")
+@Entity//当前类是一个实体类
+@Table(name="employee")//当前实体类对应哪一张表名称
 public class Employee implements Serializable{
+	
+	@Id//表示当前属性是主键
+	@Column(name="eid",unique=true,nullable=false)//指定当前属性对应表中的哪一列
+	@GeneratedValue(strategy=GenerationType.IDENTITY)//指定主键生成策略
 	private int eid;
 	
 	//员工姓名
+	@Column(name="ename",length=20,nullable=false)
 	private String ename;
 	
 	//职位名称
+	@Column(name="jobid")
 	private int jobid;
 	
 	//直属领导人
+	@Column(name="managerid")
 	private int managerid;
 	
 	//入职时间
+	@Column(name="hireDate")
 	private Date hireDate;
 	
 	//工资
+	@Column(name="salary")
 	private int salary;
 	
 	//津贴
+	@Column(name="comm")
 	private int comm;
 	
 	//所属部门
+	@Column(name="deptid")
 	private int deptid;
 
 	public Employee() {}
--- src/com/web/entity/Role.java
+++ src/com/web/entity/Role.java
@@ -2,12 +2,30 @@ package com.web.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+
 @SuppressWarnings("serial")
+@Entity
+@Table(name="role")
 public class Role implements Serializable{
 
+	@Id
+	@Column(name="rid",unique=true,nullable=false)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int rid;
 	
+	@Column(name="rname",length=20,unique=true,nullable=false)
 	private String rname;
+	
+	@Transient
+	private String test;
 
 	public Role() {}
 
@@ -34,6 +52,14 @@ public class Role implements Serializable{
 
 	public void setRname(String rname) {
 		this.rname = rname;
+	}
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
 	}
 	
 }
